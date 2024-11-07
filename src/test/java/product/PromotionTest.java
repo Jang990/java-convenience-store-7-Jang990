6e552174f87030093441ed877255a375f4e48402
Jang <sdsd090811@gmail.com>
2024-11-07 21:20:04 +0900

    test: 프로모션 기간에 따라 이용가능 여부 판단 테스트 추가

---
+++ src/test/java/product/PromotionTest.java
@@ -0,0 +1,33 @@
+package product;
+
+import camp.nextstep.edu.missionutils.DateTimes;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.time.LocalDate;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PromotionTest {
+    static final LocalDate now = LocalDate.from(DateTimes.now());
+
+    @DisplayName("현재 시간이 프로모션 기간에 포함된 경우 이용가능하다.")
+    @ParameterizedTest(name = "프로모션 기간 : {0} ~ {1}")
+    @MethodSource("promotionDurationOptions")
+    void test1(LocalDate startDate, LocalDate endDate, boolean expected) {
+        Promotion promotion = new Promotion(startDate, endDate);
+        assertEquals(promotion.isAvailable(), expected);
+    }
+
+    static Stream<Arguments> promotionDurationOptions() {
+        return Stream.of(
+                Arguments.of(now.minusDays(1), now.plusDays(1), true),
+                Arguments.of(now.minusDays(0), now.plusDays(0), true),
+                Arguments.of(now.minusDays(1), now.minusDays(1), false)
+        );
+    }
+
+}
