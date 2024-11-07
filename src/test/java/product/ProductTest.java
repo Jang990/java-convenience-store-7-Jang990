ed899565b8352d95de7def73f067570aa14c763a
Jang <sdsd090811@gmail.com>
2024-11-07 20:38:41 +0900

    test: 제품 프로모션 재고 예외 발생 테스트 추가

--- src/test/java/product/ProductTest.java
+++ src/test/java/product/ProductTest.java
@@ -13,4 +13,12 @@ class ProductTest {
         assertThrows(IllegalStateException.class, () -> product.sale(11));
     }
 
+    @DisplayName("프로모션을 적용할 수 있지만, 프로모션 재고가 없다면 예외가 발생한다.")
+    @Test
+    void test2() {
+        Promotion promotion = new Promotion();
+        Product product = new Product(10, 0, promotion);
+        assertThrows(PromotionException.class, () -> product.sale(1));
+    }
+
 }
