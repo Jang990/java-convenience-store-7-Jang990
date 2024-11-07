69fbfc83734df9f80fffe4c1a42dbeb2509ef792
Jang <sdsd090811@gmail.com>
2024-11-07 15:56:52 +0900

    test: MarkDownFileReader.read 다른 파일 읽기 테스트 추가

--- src/test/java/file/MarkDownFileReaderTest.java
+++ src/test/java/file/MarkDownFileReaderTest.java
@@ -2,32 +2,43 @@ package file;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MarkDownFileReaderTest {
     @DisplayName(".md 파일을 읽어올 수 있다.")
-    @Test
-    void test1() {
+    @ParameterizedTest(name = "{0} 경로의 파일을 읽어온다.")
+    @MethodSource("readingFileOptions")
+    void test1(String path, List<Map<String,String>> expected) {
         MarkDownFileReader reader = new MarkDownFileReader();
-        String path = "classspath:TestProduct.md";
-
         List<Map<String, String>> result = reader.read(path);
 
-        assertEquals(result.size(), 2);
-        assertEquals(result, List.of(
-                Map.of(
-                        "ID","1",
-                        "이름", "콜라"
+        assertEquals(result.size(), expected.size());
+        assertEquals(result, expected);
+    }
+
+    static Stream<Arguments> readingFileOptions() {
+        return Stream.of(
+                Arguments.of("classspath:TestProduct.md",
+                        List.of(
+                                Map.of("ID","1", "이름", "콜라"),
+                                Map.of("ID","2", "이름", "컵라면")
+                        )
                 ),
-                Map.of(
-                        "ID","2",
-                        "이름", "컵라면"
+                Arguments.of("classspath:TestPromotions.md",
+                        List.of(
+                                Map.of("name","탄산2+1", "start_date", "2024-01-01"),
+                                Map.of("name","MD추천상품", "start_date", "2024-12-31")
+                        )
                 )
-        ));
+        );
     }
 
 }
