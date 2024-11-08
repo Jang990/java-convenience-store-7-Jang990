f22ea6a0e05b335d995c0d3945284e7e0837339f
Jang <sdsd090811@gmail.com>
2024-11-08 16:53:12 +0900

    refactor: 테스트용 프로모션 빌더에서 condition을 직접받도록 수정

--- src/test/java/product/PromotionTestBuilder.java
+++ src/test/java/product/PromotionTestBuilder.java
@@ -1,58 +1,48 @@
 package product;
 
 public class PromotionTestBuilder {
-    public static final int DEFAULT_REQUIRED_QUANTITY = 2;
-    public static final int DEFAULT_FREE_QUANTITY = 1;
+    public static final PromotionCondition DEFAULT_CONDITION = new PromotionCondition(new Quantity(1), new Quantity(1));
     public static final PromotionDuration DEFAULT_DURATION = PromotionDurationStub.withInPeriod;
     public static final String DEFAULT_NAME = "이벤트";
 
-    private int requiredQuantity;
-    private int freeQuantity;
+    private PromotionCondition condition;
     private PromotionDuration duration;
     private String name;
 
-    protected PromotionTestBuilder(int requiredQuantity, int freeQuantity, PromotionDuration duration, String name) {
-        this.requiredQuantity = requiredQuantity;
-        this.freeQuantity = freeQuantity;
+    protected PromotionTestBuilder(PromotionCondition condition, PromotionDuration duration, String name) {
+        this.condition = condition;
         this.duration = duration;
         this.name = name;
     }
 
     public static PromotionTestBuilder builder() {
         return new PromotionTestBuilder(
-                DEFAULT_REQUIRED_QUANTITY,
-                DEFAULT_FREE_QUANTITY,
+                DEFAULT_CONDITION,
                 DEFAULT_DURATION,
                 DEFAULT_NAME
         );
     }
 
-    public PromotionTestBuilder condition(int requiredQuantity, int freeQuantity) {
+    public PromotionTestBuilder condition(PromotionCondition condition) {
         return new PromotionTestBuilder(
-                requiredQuantity, freeQuantity, duration, name
+                condition, duration, name
         );
     }
 
     public PromotionTestBuilder name(String name) {
         return new PromotionTestBuilder(
-                requiredQuantity, freeQuantity, duration, name
+                condition, duration, name
         );
     }
 
     public PromotionTestBuilder duration(PromotionDuration duration) {
         return new PromotionTestBuilder(
-                requiredQuantity, freeQuantity, duration, name
+                condition, duration, name
         );
     }
 
     public Promotion build() {
-        return new Promotion(
-                new PromotionCondition(
-                        new Quantity(requiredQuantity),
-                        new Quantity(freeQuantity)
-                ),
-                duration, name
-        );
+        return new Promotion(condition, duration, name);
     }
 
 }
