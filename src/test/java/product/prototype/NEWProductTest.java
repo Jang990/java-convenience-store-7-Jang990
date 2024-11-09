f24adf55618271d6ddd8f1bb54a13c7d6c161407
Jang <sdsd090811@gmail.com>
2024-11-09 11:55:41 +0900

    refactor: 재고 감소 로직 테스트를 ProductQuantity로 파라미터 수정

--- src/test/java/product/prototype/NEWProductTest.java
+++ src/test/java/product/prototype/NEWProductTest.java
@@ -19,10 +19,10 @@ class NEWProductTest {
     }
 
     @DisplayName("프로모션 재고가 부족할 때 일반 재고를 사용한다.")
-    @ParameterizedTest(name = "상품({0}.{1})을 {2}개 구매 : 상품 재고 - {3}.{4}")
+    @ParameterizedTest(name = "{0}을 {1}개 구매 : 상품 재고 - {2}.{3}")
     @MethodSource("buyOptions")
-    void test2(int promotion, int normal, int buyQuantity, int expectedPromotion, int expectedNormal) {
-        NEWProduct product = new NEWProduct(promotion,normal);
+    void test2(ProductQuantity productQuantity, int buyQuantity, int expectedPromotion, int expectedNormal) {
+        NEWProduct product = new NEWProduct(productQuantity.promotion, productQuantity.normal);
         product.buy(buyQuantity);
 
         assertEquals(expectedPromotion, product.getPromotionStock());
@@ -30,12 +30,28 @@ class NEWProductTest {
     }
 
     static Stream<Arguments> buyOptions() {
+        ProductQuantity quantity = new ProductQuantity(2, 2);
         return Stream.of(
-                Arguments.of(2, 2, 1, 1, 2),
-                Arguments.of(2, 2, 2, 0, 2),
-                Arguments.of(2, 2, 3, 0, 1),
-                Arguments.of(2, 2, 4, 0, 0)
+                Arguments.of(quantity, 1, 1, 2),
+                Arguments.of(quantity, 2, 0, 2),
+                Arguments.of(quantity, 3, 0, 1),
+                Arguments.of(quantity, 4, 0, 0)
         );
+    }
+
+    static class ProductQuantity {
+        public int promotion;
+        public int normal;
+
+        public ProductQuantity(int promotion, int normal) {
+            this.promotion = promotion;
+            this.normal = normal;
+        }
+
+        @Override
+        public String toString() {
+            return "상품재고 {" + "프로모션=" + promotion + ", 일반=" + normal + '}';
+        }
     }
 
 }
