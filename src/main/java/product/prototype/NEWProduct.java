ca675681f0bff4b3fcc51917f909932c817e424b
Jang <sdsd090811@gmail.com>
2024-11-09 11:34:30 +0900

    feat: 상품 재고 감소 단순 로직 추가

--- src/main/java/product/prototype/NEWProduct.java
+++ src/main/java/product/prototype/NEWProduct.java
@@ -2,13 +2,31 @@ package product.prototype;
 
 public class NEWProduct {
     private int stock;
+    
+    private int promotionStock;
+    private int normalStock;
 
     public NEWProduct(int stock) {
         this.stock = stock;
+    }
+    public NEWProduct(int promotionStock, int normalStock) {
+        this.promotionStock = promotionStock;
+        this.normalStock = normalStock;
     }
 
     public void buy(int quantity) {
         if(stock < quantity)
             throw new IllegalStateException("재고가 부족합니다.");
+
+        promotionStock -= 2;
+        normalStock -= 1;
+    }
+
+    public int getPromotionStock() {
+        return promotionStock;
+    }
+
+    public int getNormalStock() {
+        return normalStock;
     }
 }
