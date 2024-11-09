664b67876b663e6531567749a18269670db1f2e7
Jang <sdsd090811@gmail.com>
2024-11-09 11:23:12 +0900

    refactor: 재구성하는 클래스 프로토타입 패키지로 이동

--- src/main/java/product/prototype/NEWProduct.java
+++ src/main/java/product/prototype/NEWProduct.java
@@ -1,4 +1,4 @@
-package product;
+package product.prototype;
 
 public class NEWProduct {
     private int stock;
--- src/test/java/product/prototype/NEWProductTest.java
+++ src/test/java/product/prototype/NEWProductTest.java
@@ -1,4 +1,4 @@
-package product;
+package product.prototype;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
