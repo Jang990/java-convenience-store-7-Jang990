b3484c772debe63b520b62b61d17e8e000cd5abe
Jang <sdsd090811@gmail.com>
2024-11-09 11:29:40 +0900

    test: 프로모션 재고 우선 감소 테스트 추가

--- src/test/java/product/prototype/NEWProductTest.java
+++ src/test/java/product/prototype/NEWProductTest.java
@@ -13,4 +13,14 @@ class NEWProductTest {
         assertThrows(IllegalStateException.class, () -> product.buy(11));
     }
 
+    @DisplayName("프로모션 재고가 부족할 때 일반 재고를 사용한다.")
+    @Test
+    void test2() {
+        NEWProduct product = new NEWProduct(2,2);
+        product.buy(3);
+
+        assertEquals(0, product.getPromotionStock());
+        assertEquals(1, product.getNormalStock());
+    }
+
 }
