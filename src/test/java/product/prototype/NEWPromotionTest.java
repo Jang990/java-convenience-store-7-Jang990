9a7e0b675b26468d5548318714610bb09d1ce1a4
Jang <sdsd090811@gmail.com>
2024-11-10 11:56:22 +0900

    test: 재구성 중인 프로모션에서 적게 구매한 경우 예외 테스트 추가

--- src/test/java/product/prototype/NEWPromotionTest.java
+++ src/test/java/product/prototype/NEWPromotionTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import product.PromotionDuration;
 import product.PromotionDurationStub;
+import product.exception.PromotionException;
 
 import java.time.LocalDate;
 
@@ -36,6 +37,26 @@ class NEWPromotionTest {
                 .build();
 
         assertEquals(promotion.toString(), promotionName);
+    }
+
+    @DisplayName("프로모션 단위보다 적게 구매할 시 예외가 발생하고 얼마나 부족한지를 알려준다.")
+    @Test
+    void test3() {
+        NEWPromotion promotion = NEWPromotionTestBuilder.builder()
+                .type(PromotionType.TWO_PLUS_ONE)
+                .build();
+        ProductQuantity requested = toProductQuantity(1, 0);
+
+        PromotionException exception = assertThrows(PromotionException.class, () -> promotion.calculateFree(requested));
+        assertEquals(toQuantity(2), exception.getErrorQuantity());
+    }
+
+    private ProductQuantity toProductQuantity(int promotion, int normal) {
+        return new ProductQuantity(toQuantity(promotion), toQuantity(normal));
+    }
+
+    private Quantity toQuantity(int quantity) {
+        return new Quantity(quantity);
     }
 
 }
