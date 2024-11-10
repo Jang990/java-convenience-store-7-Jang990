4038f34ff54f1c9c8a1e9b4a0ecc13e02cc900b7
Jang <sdsd090811@gmail.com>
2024-11-10 16:00:58 +0900

    refactor: 프로모션 번들이 프로모션 타입을 생성자로 받도록 테스트 코드 변경

--- src/main/java/product/prototype/PromotionBundles.java
+++ src/main/java/product/prototype/PromotionBundles.java
@@ -3,11 +3,20 @@ package product.prototype;
 public class PromotionBundles extends Quantity {
     private final Quantity remainder;
     private final Quantity unit;
+    private final PromotionType appliedPromotion;
+
+    public PromotionBundles(Quantity base, PromotionType appliedPromotion) {
+        super(base.amount / appliedPromotion.getAppliedUnit().amount);
+        this.unit = appliedPromotion.getAppliedUnit();
+        this.remainder = new Quantity(base.amount % unit.amount);
+        this.appliedPromotion = appliedPromotion;
+    }
 
     public PromotionBundles(Quantity base, Quantity unit) {
         super(base.amount / unit.amount);
         remainder = new Quantity(base.amount % unit.amount);
         this.unit = unit;
+        appliedPromotion = null;
     }
 
     public boolean hasRemainder() {
--- src/test/java/product/prototype/PromotionBundlesTest.java
+++ src/test/java/product/prototype/PromotionBundlesTest.java
@@ -9,15 +9,15 @@ class PromotionBundlesTest {
     @DisplayName("남는 수량이 있는지 확인할 수 있다.")
     @Test
     void test1() {
-        assertTrue(new PromotionBundles(toQuantity(10), toQuantity(3)).hasRemainder());
-        assertFalse(new PromotionBundles(toQuantity(10), toQuantity(2)).hasRemainder());
+        assertTrue(new PromotionBundles(toQuantity(10), PromotionType.TWO_PLUS_ONE).hasRemainder());
+        assertFalse(new PromotionBundles(toQuantity(10), PromotionType.ONE_PLUS_ONE).hasRemainder());
     }
 
     @DisplayName("나머지 없는 번들을 만드는데 부족한 수량이 얼마인지 알 수 있다.")
     @Test
     void test2() {
-        assertEquals(new PromotionBundles(toQuantity(10), toQuantity(3)).getShortFall(), toQuantity(2));
-        assertEquals(new PromotionBundles(toQuantity(10), toQuantity(2)).getShortFall(), toQuantity(0));
+        assertEquals(new PromotionBundles(toQuantity(10), PromotionType.TWO_PLUS_ONE).getShortFall(), toQuantity(2));
+        assertEquals(new PromotionBundles(toQuantity(10), PromotionType.ONE_PLUS_ONE).getShortFall(), toQuantity(0));
     }
 
     private Quantity toQuantity(int quantity) {
