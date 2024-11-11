2abe15970f66035a23bdc696556bee1186c96aed
Jang <sdsd090811@gmail.com>
2024-11-11 13:33:52 +0900

    test: 상품 파일 정보를 읽어오는 테스트 코드 추가

--- src/test/java/store/StoreFileReaderTest.java
+++ src/test/java/store/StoreFileReaderTest.java
@@ -1,10 +1,15 @@
 package store;
 
 import file.HeaderFileReaderStub;
+import money.Money;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import product.Product;
+import product.ProductQuantity;
 import product.Promotion;
+import product.Quantity;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
@@ -28,6 +33,32 @@ class StoreFileReaderTest {
                     "end_date", "2024-11-30"
             )
     );
+
+    /*
+    name,price,quantity,promotion
+    콜라,1000,10,탄산2+1
+    콜라,1000,10,null
+    사이다,1000,8,탄산2+1
+     */
+    public final Map<String, String> product1 = Map.of(
+            "name", "콜라",
+            "price", "1000",
+            "quantity", "10",
+            "promotion", "탄산2+1"
+    );
+    public final Map<String, String> product2 = Map.of(
+            "name", "콜라",
+            "price", "1000",
+            "quantity", "10",
+            "promotion", "null"
+    );
+    public final Map<String, String> product3 = Map.of(
+            "name", "사이다",
+            "price", "1000",
+            "quantity", "7",
+            "promotion", "null"
+    );
+    public final List<Map<String, String>> sampleProductData = List.of(product1, product2, product3);
 
     @DisplayName("프로모션 파일을 읽어옵니다.")
     @Test
@@ -58,6 +89,21 @@ class StoreFileReaderTest {
         List<Map<String, String>> sample = List.of(Map.of("name", "aaa", "buy", "2", "get", "k"));
         StoreFileReader reader = createStoreFileReader(sample);
         assertThrows(IllegalArgumentException.class, reader::readPromotions);
+    }
+
+    @DisplayName("상품 파일 정보를 읽어옵니다.")
+    @Test
+    void test4() {
+        List<Promotion> promotions = createStoreFileReader(samplePromotionData).readPromotions();
+        StoreFileReader reader = createStoreFileReader(sampleProductData);
+
+        List<Product> products = reader.readProduct(promotions);
+
+        assertEquals(products.size(), 2);
+        assertEquals(products.get(0).getName(), "콜라");
+        assertEquals(products.get(0).getPrice(), new Money(1000));
+        assertEquals(products.get(0).getStock(), new ProductQuantity(new Quantity(10), new Quantity(10)));
+        assertEquals(products.get(1).getName(), "사이다");
     }
 
     private static StoreFileReader createStoreFileReader(List<Map<String, String>> sample) {
